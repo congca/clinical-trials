0b138bae014d7ae1e03838de15dde6e367dba8f6
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-10-12 16:11:51 -0400

    add simple example

---
+++ example/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "github.com/fjukstad/clinical"
+
+func main() {
+	fmt.Println(clinical.Search("breast cancer",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""))
+}
